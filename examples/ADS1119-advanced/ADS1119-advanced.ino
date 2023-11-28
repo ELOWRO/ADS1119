503f810e16ddcf0a2d971421421f8fc65339b62f
MarcoCiau <marcoantoniociau@hotmail.com>
2023-11-28 11:47:40 -0600

    update examples

--- examples/ADS1119-advanced/ADS1119-advanced.ino
+++ examples/ADS1119-advanced/ADS1119-advanced.ino
@@ -3,19 +3,11 @@
 
 ADS1119 ads1 = ADS1119(byte(0B1000001));
 
-struct ADS1119Configuration calibration;
 struct ADS1119Configuration configuration;
 
 void setup() {
   Serial.begin(9600);
-
-  calibration.mux = ADS1119MuxConfiguration::positiveAIN2negativeAGND; // VREF connected to AIN2 !!!
-  calibration.gain = ADS1119Configuration::Gain::one;
-  calibration.dataRate  = ADS1119Configuration::DataRate::sps20;
-  calibration.conversionMode = ADS1119Configuration::ConversionMode::continuous;
-  calibration.voltageReference = ADS1119Configuration::VoltageReferenceSource::external;
-  calibration.externalReferenceVoltage = 5.0;
-
+  
   configuration.mux = ADS1119MuxConfiguration::positiveAIN0negativeAIN1;
   configuration.gain = ADS1119Configuration::Gain::one;
   configuration.dataRate  = ADS1119Configuration::DataRate::sps20;
@@ -27,9 +19,10 @@ void setup() {
 
   ads1.begin();
   ads1.reset();
-
-  ads1.performOffsetCalibration(calibration);
-  
+  // Offset Calibration: VREF connected to AIN2 !!!
+  // ads1.performOffsetCalibration(ADS1119MuxConfiguration::positiveAIN2negativeAGND);
+  // Offset Calibration: shorting the inputs to AVDD / 2 !!!
+  ads1.performOffsetCalibration(ADS1119MuxConfiguration::shortedToHalvedAVDD);
   wdt_enable(WDTO_2S);
   Serial.println("setup done");
 }
